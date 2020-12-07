1dc536cdf823f81463078a66728c5ab9d00832d6
JChris246 <chris.401@live.com>
2020-12-07 13:41:42 -0400

    Implemented Auth
    login user:
    	updates the current user context
    	places the current user in local storage
    Added function to determine if user is logged in

--- src/context/Auth.js
+++ src/context/Auth.js
@@ -8,9 +8,12 @@ export const AuthProvider = ({ children }) => {
     const loginUser = (data) => {
         //TODO: Handle login functionality here
         //TODO: Store login info in localstorage (don't store password!)
+        setCurrentUser(data);
+        localStorage.setItem("user", data);
     }
+    const isLoggedIn = () => localStorage.getItem("user");
     return (
-        <AuthContext.Provider value={{ currentUser, loginUser }} >
+        <AuthContext.Provider value={{ currentUser, loginUser, isLoggedIn}} >
             { children}
         </AuthContext.Provider >
     )
