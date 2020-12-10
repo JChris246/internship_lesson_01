26050df04d5e7b9b003707fb9bd6344095457061
JChris246 <chris.401@live.com>
2020-12-10 17:27:17 -0400

    feat: Use react-hook-form (and yup) to add form validation

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -2,11 +2,21 @@ import { useContext } from 'react'
 
 import { AuthContext } from '../context/Auth'
 import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 
 const Login = () => {
 
     const { loginUser } = useContext(AuthContext)
-    const {register, handleSubmit} = useForm();
+
+    const formValidationSchema = Yup.object ({
+        email: Yup.string().email("Not a valid email").required("Email is required"),
+        password: Yup.string().min(8, "Your password must be at least 8 chars long")
+            .required("Password is required")
+    })
+    const {register, handleSubmit, errors} = useForm({
+        resolver: yupResolver(formValidationSchema),
+    });
 
     const processSubmit = (data) => {
         //TODO: Handle form submission
@@ -18,13 +28,15 @@ const Login = () => {
     return (
         <div>
             <form onSubmit={handleSubmit(processSubmit)} style={{ display: 'flex', flexDirection: 'column' }}>
-                <label htmlFor="username" style={{ marginTop: 5 }}>
-                    Username
-                    <input type="text" name="username" ref={register} />
+                <label htmlFor="email" style={{ marginTop: 5 }}>
+                    Email
+                    <input type="email" name="email" ref={register} />
+                    {errors.email && <span>{errors.email.message}</span>}
                 </label>
                 <label htmlFor="password" style={{ marginTop: 5 }}>
                     Password
                     <input type="password" name="password" ref={register} />
+                    {errors.password && <span>{errors.password.message}</span>}
                 </label>
                 <div style={{ marginTop: 5 }}>
                     <button type='submit'>Login Now</button>
