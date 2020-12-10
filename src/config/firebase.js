63e916c09be320b66fb00de0c0aea2727bff45db
JChris246 <chris.401@live.com>
2020-12-10 08:35:45 -0400

    Abstract firebase config to use personal keys from env file

---
+++ src/config/firebase.js
@@ -0,0 +1,36 @@
+import firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+// import "firebase/auth";
+import "firebase/firestore";
+import "firebase/auth";
+
+var firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID
+};
+
+// Initialize Firebase
+try {
+    firebase.initializeApp(firebaseConfig);
+} catch (err) {
+    console.log('Firebase already initialized')
+}
+
+const db = firebase.firestore()
+const auth = firebase.auth()
+const provider = new firebase.auth.GithubAuthProvider().setCustomParameters({
+    'allow_signup': 'false'
+});
+  
+
+export const firebaseObj = {
+    db: db, 
+    auth: auth,
+    githubProvider: provider
+};
